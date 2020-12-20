a3b1eb52ef53ea0ed892c557cadce3c328d213f7
typicalchazz <chase.ryan.dalton@gmail.com>
2020-12-20 16:28:42 -0600

    Add print_board

---
+++ Sudoku_Solver.py
@@ -0,0 +1,20 @@
+board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+[6, 0, 0, 1, 9, 5, 0, 0, 0],
+[0, 9, 8, 0, 0, 0, 0, 6, 0],
+[8, 0, 0, 0, 6, 0, 0, 0, 3],
+[4, 0, 0, 8, 0, 3, 0, 0, 1],
+[7, 0, 0, 0, 2, 0, 0, 0, 6],
+[0, 6, 0, 0, 0, 0, 2, 8, 0],
+[0, 0, 0, 4, 1, 9, 0, 0, 5],
+[0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+def print_board(board):
+    if board == []:
+        print("Not a Valid Board")
+    for line in board:
+        line_to_print = ""
+        for word in line:
+            line_to_print += str(word)
+        print(line_to_print)
+
+print_board(board)
