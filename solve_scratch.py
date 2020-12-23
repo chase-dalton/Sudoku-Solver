6d6b18bcc61a341e5aee44994b87b9b1b2cc3c78
typicalchazz <chase.ryan.dalton@gmail.com>
2020-12-23 15:47:13 -0600

    Worked with Michael

--- solve_scratch.py
+++ solve_scratch.py
@@ -1,12 +1,12 @@
-board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-[6, 0, 0, 1, 9, 5, 0, 0, 0],
-[0, 9, 8, 0, 0, 0, 0, 6, 0],
-[8, 0, 0, 0, 6, 0, 0, 0, 3],
-[4, 0, 0, 8, 0, 3, 0, 0, 1],
-[7, 0, 0, 0, 2, 0, 0, 0, 6],
-[0, 6, 0, 0, 0, 0, 2, 8, 0],
-[0, 0, 0, 4, 1, 9, 0, 0, 5],
-[0, 0, 0, 0, 8, 0, 0, 7, 9]]
+board =[[5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 
 def print_board(board):
@@ -14,8 +14,8 @@ def print_board(board):
         print("Not a Valid Board")
     for line in board:
         line_to_print = ""
-        for word in line:
-            line_to_print += str(word)
+        for num in line:
+            line_to_print += str(num)
         print(line_to_print)
         #return line_to_print
 
@@ -23,7 +23,7 @@ def find_zero(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
-                return(row,col)
+                return[row,col]
                 
 
 
@@ -49,7 +49,8 @@ def is_valid(board,row, col, value):
                 if item == value:
                     return False
                     #return "Value is in the column!"
-    
+
+    #checking if the value is in the same 3x3 grid
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
             if board[i][j] == value and i != row and j != col:
@@ -58,34 +59,37 @@ def is_valid(board,row, col, value):
     
 
 
-def print_col(board,col):
-    return [row[col] for row in board]
-        
-
-print_board(board)
-#print(find_zero(board2))
-#print(is_valid(board, 0, 2, 1))
-#print(is_valid(board, 0, 2, 7))
-#print(is_valid(board, 0, 2, 6))
-#print(is_valid(board, 3, 5, 9))
-
-#print(print_col(board2,2))
 
 
-def solve(board):
+#def print_col(board,col):
+    #return [row[col] for row in board]
+
+def solve_square(board):
     find = find_zero(board)
     if not find:
         return True
     else:
         row, col = find
-    for i in range(1,10):
-        if is_valid(board,row,col,i):
-            board[row][col] = i
-            if solve(board):
+        valid_pos = [x for x in range(1,10) if is_valid(board, row, col, x)]
+        for value in valid_pos:
+            board[row][col] = value
+            if solve_square(board):
                 return True
-            
-            board[row][col] = 0
+        board[row][col] == 0
+        return False
 
-    return False
 
-print(solve(board))
+
+
+
+
+
+
+
+
+
+#print_board(board)
+#print(solve_square(board))
+#print_board(board)
+
+print(for x in range(1,10))
